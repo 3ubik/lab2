52258e9e1a2916108c973fbc1249d41e38d0bf63
Approxomiron <nzubtsov194@gmail.com>
2019-11-09 18:11:18 +0300

    add 2laba

---
+++ src/lab2/mainapplication.java
@@ -0,0 +1,9 @@
+package lab2;
+import javax.swing.JFrame;
+public class mainapplication {
+	public static void main(String[]args) {
+	mainframe frame = new mainframe();
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setVisible(true);
+	}
+}
